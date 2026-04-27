f6c40d1cce8c6d0c136e760445b14ba330ece099
elias-aymonier <elias.aymonier.pro@gmail.com>
2026-04-27 15:23:56 +0200

    Teste install waybar

--- waybar-install.sh
+++ waybar-install.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
 
-FLAG="$HOME/.config/omarchy-theme/.waybar_checked"
-
-if [ ! -f "$FLAG" ]; then
-    touch "$FLAG"
-
-    if ! command -v waybar &> /dev/null; then
-        choice=$(echo -e "Install Waybar\nSkip" | rofi -dmenu -p "Waybar is missing")
-
-        if [[ "$choice" == "Install Waybar" ]]; then
-            echo "sudo git clone https://github.com/aymonier-elias/outer-wilds-theme /tmp/repo && cp -rf /tmp/repo/waybar-theme/. ~/.config/waybar && rm -rf /tmp/repo && omarchy-restart-waybar"
-        fi
-    fi
-fi
+sudo git clone https://github.com/aymonier-elias/outer-wilds-theme /tmp/repo && cp -rf /tmp/repo/waybar-theme/. ~/.config/waybar && sudo rm -rf /tmp/repo && omarchy-restart-waybar
